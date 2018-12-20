f16dc2c63ab2229b58d854c15870ef5d3b894030
Lewis Grozinger <l.grozinger2@ncl.ac.uk>
2018-12-20 14:51:14 +0000

    Single-threaded differential evolution tested with ROSENBROCK.
    DE/RAND/1/BIN, DE/BEST/1/BIN, DE/RAND/2/BIN and DE/BEST/2/BIN.

--- src/clde.lisp
+++ src/clde.lisp
@@ -1,9 +1,219 @@
 (defpackage clde
   (:use :cl)
-  (:export :pick-random))
+  (:export :de/rand/1/bin
+	   :de/rand/2/bin
+	   :de/best/2/bin
+	   :de/best/1/bin
+	   :pick-random
+	   :choose-n
+	   :choose-n-not
+	   :v-add
+	   :v-sub
+	   :v-scale
+	   :f-diff
+	   :gen-donor
+	   :crossover
+	   :select
+	   :random-individual
+	   :random-population
+	   :rosenbrock))
 (in-package :clde)
 
 ;; blah blah blah.
 
-(defun pick-random (list)
-  (first list))
+(defun pick-random (vector)
+  (let ((rand-index (random (length vector))))
+    (aref vector rand-index)))
+
+(defun choose-n (vector n)
+  (let ((chosen (make-array n))
+	(remaining (make-array (length vector) :initial-contents vector)))
+    (loop
+       :for i :from 0 :below n
+       :do
+       (setf (aref chosen i) (pick-random remaining))
+       (setf remaining (remove (aref chosen i) remaining :count 1 :test #'equalp)))
+
+    chosen))
+
+(defun choose-n-not (vector n but-not)
+  (let ((allowed (remove but-not vector :count 1 :test #'equalp)))
+    (choose-n allowed n)))
+
+(defun v-add (vector-a vector-b)
+  (map 'simple-vector #'+ vector-a vector-b))
+
+(defun v-sub (vector-a vector-b)
+  (map 'simple-vector #'- vector-a vector-b))
+
+(defun v-scale (factor vector)
+  (map 'simple-vector #'(lambda (x) (* factor x)) vector))
+
+(defun f-diff (f vector-a vector-b)
+  (v-scale f (v-sub vector-a vector-b)))
+
+(defun gen-donor (eligible-population f &key (diffs 1))
+  (let* ((chosen (choose-n eligible-population (+ 1 (* diffs 2))))
+	 (donor (aref chosen 0)))
+    (loop
+       :for i :from 1 :below (length chosen) :by 2
+       :do
+       (setf donor (v-add donor (f-diff f (aref chosen i) (aref chosen (+ 1 i))))))
+
+    donor))
+
+(defun gen-donor-best (best eligible-population f &key (diffs 1))
+  (let* ((chosen (choose-n eligible-population (* diffs 2)))
+	 (donor (make-array (length best) :initial-contents best)))
+    (loop
+       :for i :from 0 :below (length chosen) :by 2
+       :do
+       (setf donor (v-add donor (f-diff f (aref chosen i) (aref chosen (+ 1 i))))))
+
+    donor))
+
+
+(defun crossover (target-vector donor-vector crossover-rate)
+  (let* ((d (length target-vector))
+	 (rand-parameter (random d))
+	 (trial-vector (make-array d :initial-contents target-vector)))
+
+    (loop
+       :for j :from 0 :below d
+       :do
+       (if (or (<= (random 1.0d0) crossover-rate) (eql j rand-parameter))
+	   (setf (aref trial-vector j) (aref donor-vector j))))
+
+    trial-vector))
+
+(defun rosenbrock (x)
+  (let ((d (length x)))
+    (loop
+       :for i :below (1- d)
+       :summing (+ (* 100
+		      (expt (- (aref x (1+ i))
+			       (expt (aref x i) 2))
+			    2))
+		   (expt (- 1 (aref x i)) 2)))))
+
+(defun select (vector-a vector-b cost-function)
+  (if (< (funcall cost-function vector-a)
+	 (funcall cost-function vector-b))
+      vector-a
+      vector-b))
+
+(defun random-individual (dimensions upper lower)
+  (make-array dimensions :initial-contents (loop
+					      :for i :from 0 :below dimensions
+					      :collect (+ lower (random (- upper lower))))))
+
+(defun random-population (pop-size dimensions &key (lower 0.0d0) (upper 1.0d2))
+  (make-array pop-size :initial-contents (loop
+					    :for i :from 0 :below pop-size
+					    :collect (random-individual dimensions upper lower))))
+
+(defun de/rand/1/bin (pop-size max-generations cost-function dimensions cr f)
+
+  (let* ((population (random-population pop-size dimensions))
+	 (best (aref population 0))
+	 (best-score (funcall cost-function best)))
+
+    (loop
+       :for g :from 0 :below max-generations
+       :until (eql best-score 0.0d0)
+       :do
+       (loop
+	  :for i :from 0 :below pop-size
+	  :do
+	  (let* ((target (aref population i))
+		 (donor (gen-donor (choose-n-not population 3 target) f))
+		 (trial (crossover target donor cr))
+		 (selected (select target trial cost-function)))
+
+	    (setf (aref population i) selected)
+	    (let ((this-score (funcall cost-function selected)))
+	      (if (< this-score best-score)
+		  (progn (setf best-score this-score)
+			 (setf best selected)))))))
+
+    best))
+
+(defun de/rand/2/bin (pop-size max-generations cost-function dimensions cr f)
+
+  (let* ((population (random-population pop-size dimensions))
+	 (best (aref population 0))
+	 (best-score (funcall cost-function best)))
+
+    (loop
+       :for g :from 0 :below max-generations
+       :until (eql best-score 0.0d0)
+       :do
+       (loop
+	  :for i :from 0 :below pop-size
+	  :do
+	  (let* ((target (aref population i))
+		 (donor (gen-donor (choose-n-not population 5 target) f :diffs 2))
+		 (trial (crossover target donor cr))
+		 (selected (select target trial cost-function)))
+
+	    (setf (aref population i) selected)
+	    (let ((this-score (funcall cost-function selected)))
+	      (if (< this-score best-score)
+		  (progn (setf best-score this-score)
+			 (setf best selected)))))))
+
+    best))
+
+(defun de/best/1/bin (pop-size max-generations cost-function dimensions cr f)
+
+  (let* ((population (random-population pop-size dimensions))
+	 (best (aref population 0))
+	 (best-score (funcall cost-function best)))
+
+    (loop
+       :for g :from 0 :below max-generations
+       :until (eql best-score 0.0d0)
+       :do
+       (loop
+	  :for i :from 0 :below pop-size
+	  :do
+	  (let* ((target (aref population i))
+		 (donor (gen-donor-best best (choose-n-not population 3 target) f))
+		 (trial (crossover target donor cr))
+		 (selected (select target trial cost-function)))
+
+	    (setf (aref population i) selected)
+	    (let ((this-score (funcall cost-function selected)))
+	      (if (< this-score best-score)
+		  (progn (setf best-score this-score)
+			 (setf best selected)))))))
+
+    best))
+
+(defun de/best/2/bin (pop-size max-generations cost-function dimensions cr f)
+
+  (let* ((population (random-population pop-size dimensions))
+	 (best (aref population 0))
+	 (best-score (funcall cost-function best)))
+
+    (loop
+       :for g :from 0 :below max-generations
+       :until (eql best-score 0.0d0)
+       :do
+       (loop
+	  :for i :from 0 :below pop-size
+	  :do
+	  (let* ((target (aref population i))
+		 (donor (gen-donor-best best (choose-n-not population 5 target) f :diffs 2))
+		 (trial (crossover target donor cr))
+		 (selected (select target trial cost-function)))
+
+	    (setf (aref population i) selected)
+	    (let ((this-score (funcall cost-function selected)))
+	      (if (< this-score best-score)
+		  (progn (setf best-score this-score)
+			 (setf best selected)))))))
+
+    best))
+
+
--- tests/clde.lisp
+++ tests/clde.lisp
@@ -6,13 +6,147 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :clde)' in your Lisp.
 
-(plan 1)
+(plan 11)
 
-(let ((integer-list '(1 2 3 4 5 6 7 8 9 0)))
+(let ((integer-list (vector 1 2 3 4 5 6 7 8 9 0))
+      (symbol-list (vector 'a 'b 'c 'd 'e 'f 'g 'h 'i 'j)))
   (subtest "PICK-RANDOM tests."
+
     (is-type (pick-random integer-list) 'number
 	     "PICK-RANDOM returns same type as list members.")
-    (ok (member (pick-random integer-list) integer-list)
+
+    (is-type (pick-random symbol-list) 'symbol
+	     "PICK-RANDOM returns same type as list members.")
+
+    (ok (find (pick-random integer-list) integer-list)
 	"PICK-RANDOM returns a member of the given list.")))
+
+(let* ((integer-list (vector 1 2 3 4 5 6 7 8 9 0))
+       (chosen (choose-n integer-list 10)))
+
+  (subtest "CHOOSE-N tests."
+
+    (is (length (choose-n integer-list 1)) 1
+	"CHOOSE-N returns `n' items")
+
+    (is (length (choose-n integer-list 2)) 2
+	"CHOOSE-N returns `n' items")
+
+    (is (length (choose-n integer-list (length integer-list))) (length integer-list)
+	"CHOOSE-N returns `n' items")
+
+    (ok (subsetp (coerce (choose-n integer-list 10) 'list) (coerce integer-list 'list))
+	"CHOOSE-N returns only elements from the given list.")
+
+    (ok (eql (length (remove-duplicates chosen)) (length chosen))
+	"CHOOSE-N does not return the same element more than once.")))
+
+(let* ((integer-list (vector 1 2 3 4 5 6 7 8 9 0)))
+
+  (subtest "CHOOSE-N-NOT tests."
+
+    (is (length (choose-n-not integer-list 1 1)) 1
+	"CHOOSE-N-NOT returns `n' items")
+
+    (is (length (choose-n-not integer-list 2 1)) 2
+	"CHOOSE-N-NOT returns `n' items")
+
+    (ok (not (find 1 (choose-n-not integer-list 9 1)))
+	"CHOOSE-N-NOT does not choose `but-not'")))
+
+(subtest "V-ADD tests."
+
+  (ok (equalp (v-add #(1 2 3 4) #(1 2 3 4)) #(2 4 6 8))
+      "#(1 2 3 4) + #(1 2 3 4).")
+
+  (ok (equalp (v-add #(1 2 3 4) #(-1 -2 -3 -4)) #(0 0 0 0))
+      "#(1 2 3 4) + #(-1 -2 -3 -4).")
+
+  (ok (equalp (v-add #(0 0 0 0) #(0 0 0 0)) #(0 0 0 0))
+      "#(0 0 0 0) + #(0 0 0 0)."))
+
+(subtest "V-SUB tests."
+
+  (ok (equalp (v-sub #(1 2 3 4) #(1 2 3 4)) #(0 0 0 0))
+      "#(1 2 3 4) - #(1 2 3 4).")
+
+  (ok (equalp (v-sub #(1 2 3 4) #(-1 -2 -3 -4)) #(2 4 6 8))
+      "#(1 2 3 4) - #(-1 -2 -3 -4).")
+
+  (ok (equalp (v-sub #(0 0 0 0) #(0 0 0 0)) #(0 0 0 0))
+      "#(0 0 0 0) - #(0 0 0 0)."))
+
+(subtest "V-SCALE tests."
+
+  (ok (equalp (v-scale 0 #(1 2 3 4)) #(0 0 0 0))
+      "0 * #(1 2 3 4).")
+
+  (ok (equalp (v-scale -1 #(-1 -2 -3 -4)) #(1 2 3 4))
+      "-1 * #(-1 -2 -3 -4).")
+
+  (ok (equalp (v-scale 1 #(1 2 3 4)) #(1 2 3 4))
+      "1 * #(1 2 3 4).")
+
+  (ok (equalp (v-scale 3 #(1 2 3 4)) #(3 6 9 12))
+      "3 * #(1 2 3 4)."))
+
+(subtest "F-DIFF tests."
+
+  (ok (equalp (f-diff 0 #(1 2 3 4) #(0 0 0 0)) #(0 0 0 0))
+      "0 * (a - b).")
+
+  (ok (equalp (f-diff 1 #(1 2 3 4) #(0 0 0 0)) #(1 2 3 4))
+      "1 * (a - b).")
+
+  (ok (equalp (f-diff 2 #(1 2 3 4) #(2 3 4 5)) #(-2 -2 -2 -2))
+      "2 * (a - b)."))
+
+(subtest "GEN-DONOR tests."
+
+  (let ((population (make-array 3 :initial-contents '(#(1 2 3 4)
+						      #(0 1 2 3)
+						      #(0 1 2 3)))))
+    (ok (let ((donor (gen-donor population 0)))
+	  (or
+	   (equalp donor #(1 2 3 4))
+	   (equalp donor #(0 1 2 3))))
+	"GEN-DONOR with f = 0 gets back the first chosen vector.")
+
+    (ok (let ((donor (gen-donor population 1)))
+	  (or
+	   (equalp donor #(1 2 3 4))
+	   (equalp donor #(-1 0 1 2))))
+	"GEN-DONOR with f = 1.")
+
+    (ok (let ((donor (gen-donor population 2)))
+	  (or
+	   (equalp donor #(1 2 3 4))
+	   (equalp donor #(-2 -1 0 1))
+	   (equalp donor #(2 3 4 5))))
+	"GEN-DONOR with f = 2.")))
+
+(subtest "CROSSOVER tests."
+
+  (let ((target #(1 2 3 4))
+	(donor #(0 0 0 0)))
+
+    (ok (equalp (crossover target donor 1.0d0) #(0 0 0 0))
+	"CROSSOVER with rate 1.0d0 takes all the donor's parameters.")))
+
+(subtest "ROSENBROCK tests."
+
+  (is (rosenbrock #(1.0d0 1.0d0)) 0.0d0
+      "ROSENBROCK perfect solution in 2-d.")
+
+  (is (rosenbrock #(1.0d0 1.0d0 1.0d0)) 0.0d0
+      "ROSENBROCK perfect solution in 3-d.")
+
+  (is (rosenbrock (make-array 10 :initial-element 1.0d0)) 0.0d0
+      "ROSENBROCK perfect solution in 10-d."))
+
+(subtest "SELECT tests."
+
+  (ok (equalp (select #(0.0d0 0.0d0) #(1.0d0 1.0d0) #'rosenbrock) #(1.0d0 1.0d0))
+      "SELECT for ROSENBROCK picks perfect solution."))
 
 (finalize)
